5146c3004ec07c68694710ba6b6ece50df70113f
Sylnzy <mhasanudin317@gmail.com>
2025-08-24 17:43:31 +0700

    Update UI design: modern card-based layout with navigation system

--- backend/test_api.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('node-fetch');
-(async () => {
-    const api = 'http://localhost:4000';
-    try {
-        console.log('GET /rts');
-        console.log(await (await fetch(api + '/rts')).json());
-    } catch (e) { console.error(e) }
-})();
